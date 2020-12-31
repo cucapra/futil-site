abddd96bb1522d6fab9580aa6b6f1eff12f9febb
Samuel Thomas <sgpthomas@gmail.com>
2020-12-31 10:57:30 -0800

    moved diff editor into it's own file and cleaned up code

---
+++ js/diffEditor.js
@@ -0,0 +1,101 @@
+import * as Diff from 'diff';
+import Prism from 'prismjs';
+import './prism-futil.js';
+require('prismjs/plugins/keep-markup/prism-keep-markup');
+
+function wordDiff(diff, next, srcDiv, destDiv) {
+    // if diffs cover different number of lines,
+    // add empty lines to compensate
+    let count = diff.count - next.count;
+    if (count > 0) {
+        // more lines in diff than next
+        destDiv.appendChild(document.createTextNode('\n'.repeat(count)));
+    } else {
+        // editorStr.appendChild(document.createTextNode('\n'.repeat(-count)));
+    }
+    let wordDiff = Diff.diffWordsWithSpace(diff.value, next.value);
+    for (let change of wordDiff) {
+        if (change.added == null && change.removed == null) {
+            srcDiv.appendChild(document.createTextNode(change.value));
+            destDiv.appendChild(document.createTextNode(change.value));
+        } else if (change.added) {
+            let span = document.createElement('span');
+            span.innerHTML = change.value;
+            span.classList = 'token diff-addition';
+            destDiv.appendChild(span);
+        } else if (change.removed) {
+            let span = document.createElement('span');
+            span.innerHTML = change.value;
+            span.classList = 'token diff-deletion';
+            srcDiv.appendChild(span);
+        }
+    }
+
+}
+
+function lineDiff(diff, srcDiv, destDiv) {
+    // reset src and dest
+    srcDiv.innerHTML = "";
+    destDiv.innerHTML = "";
+    // compute line diff
+    for (let i = 0; i < diff.length; i++) {
+        let change = diff[i];
+        if (change.added == null && change.removed == null) {
+            // neither added nor removed. add to both
+            srcDiv.appendChild(
+                document.createTextNode(change.value)
+            );
+            destDiv.appendChild(
+                document.createTextNode(change.value)
+            );
+        } else if (change.removed) {
+            // check if next item is an addition
+            if (i + 1 < diff.length && diff[i + 1].added) {
+                wordDiff(change, diff[i + 1], srcDiv, destDiv);
+                // skip next iteration because we've already handled it
+                i++;
+            } else {
+                let span = document.createElement('span');
+                span.innerHTML = change.value;
+                span.classList = 'token diff-deletion';
+                srcDiv.appendChild(span);
+                if (change.count > 0) {
+                    destDiv.appendChild(document.createTextNode("\n".repeat(change.count)));
+                } else {
+                    // editorStr.appendChild(document.createTextNode("\n".repeat(change.count)));
+                }
+            }
+        } else if (change.added) {
+            // present in dest, not source
+            let span = document.createElement('span');
+            span.innerHTML = change.value;
+            span.classList.add('token');
+            span.classList.add('diff-addition');
+            destDiv.appendChild(span);
+            if (change.count > 0) {
+                let commentStr = "\n".repeat(change.count);
+                srcDiv.append(document.createTextNode(commentStr));
+            } else {
+                // let commentStr = "\n".repeat(Math.abs(change.count));
+                // compiledStr.append(document.createTextNode(commentStr));
+            }
+        }
+    }
+}
+
+export function updateDiffEditor(editor, sourceCode, compiledCode) {
+    let srcDiv = editor.querySelector("#input");
+    let destDiv = editor.querySelector("#output");
+
+    if (compiledCode.startsWith("Error:")) {
+        // there was compilation error. show that
+        destDiv.innerHTML = compiledCode;
+    } else {
+        let diff = Diff.diffLines(sourceCode, compiledCode);
+        lineDiff(diff, srcDiv, destDiv);
+
+        // syntax highlighting
+        Prism.highlightElement(srcDiv);
+        Prism.highlightElement(destDiv);
+    }
+}
--- js/index.js
+++ js/index.js
@@ -2,12 +2,7 @@ import * as calyx from "../rust/Cargo.toml";
 import data from "../examples/data.json";
 import passes from "../data/passes.json";
 import calyx_info from "../rust/calyx_hash.json";
-import * as Diff from 'diff';
-// import { CodeJar } from 'codejar';
-// import { withLineNumbers } from 'codejar/linenumbers';
-import Prism from 'prismjs';
-import './prism-futil.js';
-require('prismjs/plugins/keep-markup/prism-keep-markup');
+import { updateDiffEditor } from './diffEditor.js';
 
 var button = document.getElementById("compile");
 
@@ -63,41 +58,6 @@ button.onclick = function() {
 //     { tab: '\t' }
 // );
 
-// function filterDiff(input, prefix) {
-//     var lines = [];
-//     let hunk = input.hunks[0];
-//     console.log(hunk);
-//     // for (let i = 0; i < hunk.oldLines; i++) {
-//     for (let l of hunk.lines) {
-//         // let l = hunk.lines[i];
-//         if (l.length > 0) {
-//             let symbol = l[0];
-//             let content = l.slice(1, l.length);
-//             if (symbol == prefix) {
-//                 lines.push(content);
-//                 // lines.push(l);
-//             } else if (symbol == ' ') {
-//                 // lines.push("--space--");
-//                 lines.push(content);
-//             } else if (symbol == '\\') {
-//                 // console.log("prefix " + symbol);
-//                 // do nothing
-//             } else {
-//                 if (content.length == 0) {
-//                     lines.push('');
-//                 } else if (content.trim().startsWith("//")) {
-//                     lines.push('');
-//                 } else {
-//                     lines.push(`${symbol}${content.slice(1, l.length)}`);
-//                 }
-//             }
-//         }
-//     }
-//     // console.log(hunk.lines.slice(hunk.oldLines, hunk.lines.length));
-//     // lines = lines.concat(hunk.lines.slice(hunk.oldLines, hunk.lines.length));
-//     return lines.join('\n');
-// }
-
 function getActivePasses() {
     let list = [];
     for (let p of passes.passes) {
@@ -109,122 +69,28 @@ function getActivePasses() {
 }
 
 function compile() {
+    // get passes to run
     let passList = getActivePasses();
-    console.log(passList);
-    // let code = editor.toString();
-    let code = document.getElementById("editor").innerText;
+    // get the current code in the editor
+    let sourceCode = document.getElementById("input").innerText;
+    // cleanup input
+    sourceCode = sourceCode.replaceAll(/(\n+)(?=\s*\})/g, '\n');
+    sourceCode = sourceCode.replaceAll(/\n(\n+)(?=\s*group)/g, '\n\n');
+    // compile the code
+    var compiledCode = calyx.run(passList, library_code, sourceCode);
 
-    code = code.replaceAll(/^\+.*\n/mg, '');
-    code = code.replaceAll(/^-.*$/mg, '');
-    var str = calyx.run(passList, library_code, code);
-
-    if (str.startsWith("Error:")) {
-        document.getElementById("compiled").innerHTML = str;
-    } else {
-        let lineDiff = Diff.diffLines(code, str);
-        let compiledStr = document.getElementById("compiled");
-        compiledStr.innerHTML = "";
-        let editorStr = document.getElementById("editor");
-        editorStr.innerHTML = "";
-        for (let i = 0; i < lineDiff.length; i++) {
-            let change = lineDiff[i];
-            if (change.added == null && change.removed == null) {
-                compiledStr.appendChild(
-                    document.createTextNode(change.value)
-                );
-                editorStr.appendChild(document.createTextNode(change.value));
-            } else if (change.added) {
-                let span = document.createElement('span');
-                span.innerHTML = change.value;
-                span.classList = 'token diff-addition';
-                compiledStr.appendChild(span);
-                if (change.count > 0) {
-                    let commentStr = "\n".repeat(change.count);
-                    editorStr.append(document.createTextNode(commentStr));
-                } else {
-                    // let commentStr = "\n".repeat(Math.abs(change.count));
-                    // compiledStr.append(document.createTextNode(commentStr));
-                }
-            } else {
-                // check if next item is an addition
-                if (i + 1 < lineDiff.length && lineDiff[i + 1].added) {
-                    let next = lineDiff[i + 1];
-                    let count = change.count - next.count;
-                    if (count > 0) {
-                        compiledStr.appendChild(document.createTextNode('\n'.repeat(count)));
-                    } else {
-                        // editorStr.appendChild(document.createTextNode('\n'.repeat(Math.abs(count))));
-                    }
-                    let wordDiff = Diff.diffWordsWithSpace(change.value, next.value);
-                    for (let change of wordDiff) {
-                        if (change.added == null && change.removed == null) {
-                            editorStr.appendChild(document.createTextNode(change.value));
-                            compiledStr.appendChild(document.createTextNode(change.value));
-                        } else if (change.added) {
-                            let span = document.createElement('span');
-                            span.innerHTML = change.value;
-                            span.classList = 'token diff-addition';
-                            compiledStr.appendChild(span);
-                        } else if (change.removed) {
-                            let span = document.createElement('span');
-                            span.innerHTML = change.value;
-                            span.classList = 'token diff-deletion';
-                            editorStr.appendChild(span);
-                        }
-                    }
-                    i++;
-                } else {
-                    let span = document.createElement('span');
-                    span.innerHTML = change.value;
-                    span.classList = 'token diff-deletion';
-                    editorStr.appendChild(span);
-                    if (change.count > 0) {
-                        compiledStr.appendChild(document.createTextNode("\n".repeat(change.count)));
-                    } else {
-                        // editorStr.appendChild(document.createTextNode("\n".repeat(change.count)));
-                    }
-                }
-
-
-                // if (change.value.includes("\n")) {
-                //     compiledStr.append(document.createTextNode(change.value));
-                // }
-                // console.log(change);
-            }
-        }
-
-        Prism.highlightElement(document.getElementById('editor'));
-        Prism.highlightElement(document.getElementById('compiled'));
-        // compiled.updateCode(compiledStr);
-
-        // var diff = Diff.structuredPatch("input", "input", code, str, null, null, {
-        //     "context": code.split("\n").length,
-        // });
-        // compiled.updateCode(filterDiff(diff, '+'));
-        // editor.updateCode(filterDiff(diff, '-'));
-    }
-
-    // editor.updateCode(str);
-    // const diffHtml = Diff2Html.html(diff,
-    //     { drawFileList: false, matching: 'lines' }
-    // );
-    // compiled_div.innerHTML = diffHtml;
-    // compiled.updateCode(diff);
-    // document.getElementById('diff-pane').innerHTML = "";
-
+    // update the diff editor
+    var editor = document.getElementById("diffEditor");
+    updateDiffEditor(editor, sourceCode, compiledCode);
 }
 
 // var examples_box = document.getElementById("examples");
 var examples_select = document.getElementById("examples-select");
 examples_select.onchange = function() {
     // editor.updateCode(examples_select.value);
-    document.getElementById("editor").innerHTML = examples_select.value;
+    document.getElementById("input").innerHTML = examples_select.value;
     compile();
 };
-
-// examples_box.oninput = function() {
-//     compile();
-// };
 
 // set calyx version
 var futil_version_div = document.getElementById("calyx-version");
