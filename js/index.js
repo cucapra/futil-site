80b9e66e05983f6b1c202bae749a3315dc80f7ec
Samuel Thomas <sgpthomas@gmail.com>
2021-01-05 16:09:41 -0800

    made javascript pull sources on demand

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 import * as calyx from "../rust/Cargo.toml";
-import data from "../examples/data.json";
+// import data from "../data/examples.json";
+import config from "../data/config.json";
 import passes from "../data/passes.json";
 import calyx_info from "../rust/calyx_hash.json";
 import { updateDiffEditor, wrapLines } from './diffEditor.js';
+import 'regenerator-runtime/runtime';
 
 var button = document.getElementById("compile");
-
-var library_code = "";
-var lib_select = document.getElementById("library-select");
-lib_select.onchange = function() {
-    library_code = lib_select.value;
-};
+var libraries = {};
+var currentCode = {};
 
 function createToggle(pass) {
     let button = document.createElement("button");
@@ -36,10 +34,6 @@ for (let pass of passes.passes) {
     passDiv.appendChild(button);
 }
 
-button.onclick = function() {
-    compile();
-};
-
 function getActivePasses() {
     let list = [];
     for (let p of passes.passes) {
@@ -47,45 +41,105 @@ function getActivePasses() {
             list.push(p.name);
         }
     }
-    return list;
+    return passes.passes
+        .filter(p => p.active)
+        .map(p => p.name);
 }
 
-function cleanse(elem) {
-    let code = "";
-
-    code = elem.innerText;
-    // for (let line of elem.querySelectorAll(".line")) {
-    //     code += line.innerText;
-    // }
-
-    // cleanup input
-    code = code.replaceAll(/(\n+)(?=\s*\})/g, '\n');
-    code = code.replaceAll(/\n(\n+)(?=\s*group)/g, '\n\n');
-
-    return code;
-}
+button.onclick = function() {
+    compile();
+};
 
 function compile() {
     // get passes to run
     let passList = getActivePasses();
-    // get the current code in the editor
-    let sourceCode = cleanse(document.getElementById("input"));
+    // // get the current code in the editor
+    // let sourceCode = cleanse(document.getElementById("input"));
     // compile the code
-    var compiledCode = calyx.run(passList, library_code, sourceCode);
-
+    let libraryCode = currentCode.libraries.map(x => x.code).join("\n");
+    var compiledCode = calyx.run(
+        passList,
+        libraryCode,
+        currentCode.code
+    );
     // update the diff editor
     var editor = document.getElementById("diffEditor");
-    updateDiffEditor(editor, sourceCode, compiledCode);
+    updateDiffEditor(editor, currentCode.code, compiledCode);
 }
+
+
+async function getLibrary(library, root) {
+    if (library in libraries) {
+        return await libraries[library];
+    } else {
+        let url = `${config.url_prefix}${root}${library}`;
+        let request = await fetch(url);
+        let code = await request.text();
+        // if code has more imports, import those and append them to this lib
+        if (/import/g.test(code)) {
+            let prefix = library.split('/').slice(0, -1).join("/");
+            let names = Array.from(code.matchAll(/import "(.*)";/g)).map(x => x[1]);
+            let res = await fetchLibs(names, `${root}/${prefix}/`);
+            for (let r of res) {
+                code += r.code;
+            }
+        }
+        libraries[library] = code;
+        return code;
+    }
+}
+
+async function fetchLibs(names, root) {
+    let proms = names.map(async function(lib) {
+        let code = await getLibrary(lib, root);
+        return { name: lib, code: code };
+    });
+    return await Promise.all(proms);
+}
+
+async function getExample(name, root) {
+    let url = `${config.url_prefix}${root}${name}`;
+    let response = await fetch(url);
+    let code = await response.text();
+    let importRegex = /import "(.*)";/g;
+    let names = Array.from(code.matchAll(importRegex)).map(x => x[1]);
+    let res = await fetchLibs(names, root);
+    code = code.replaceAll(importRegex, "");
+    return {
+        code: code.trim(),
+        libraries: res,
+    };
+}
+
+var input = document.getElementById("input");
+var output = document.getElementById("output");
+function update() {
+    input.innerHTML = currentCode.code;
+}
+
+input.oninput = function() {
+    currentCode.code = input.innerText;
+};
+
+
+
+var lib_select = document.getElementById("library-select");
+lib_select.onchange = function() {
+    // library_code = getExample(lib_select.value);
+    // console.log("hi");
+};
 
 // var examples_box = document.getElementById("examples");
 var examples_select = document.getElementById("examples-select");
 examples_select.onchange = function() {
-    // editor.updateCode(examples_select.value);
-    let input = document.getElementById("input");
-    input.innerHTML = examples_select.value;
+    input.innerHTML = "loading...";
+    output.innerHTML = "loading...";
+    let value = JSON.parse(examples_select.value);
+    getExample(value.file, value.root)
+        .then(t => currentCode = t)
+        .then(() => update())
+        .then(() => compile());
     // wrapLines(input);
-    compile();
 };
 
 // set calyx version
@@ -98,15 +152,15 @@ futil_version_div.appendChild(git_link);
 
 // text
 let option;
-for (var i in data.categories) {
-    var group = data.categories[i];
+for (var i in config.categories) {
+    var group = config.categories[i];
     var sel = document.getElementById(group.name + "-select");
     if (sel) {
         for (var j in group.items) {
             var item = group.items[j];
             option = document.createElement('option');
             option.text = item.name;
-            option.value = item.content;
+            option.value = JSON.stringify(item);
             sel.add(option);
         }
         sel.onchange();
